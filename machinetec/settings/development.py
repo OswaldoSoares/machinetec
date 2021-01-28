8e7881d7e23cda91fc702407b90d054b16649aa9
OswaldoSoares <oswaldosoares@gmail.com>
2021-01-28 17:19:51 -0300

    refactor rename app user to account

--- machinetec/settings/development.py
+++ machinetec/settings/development.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'a(o25lb!%pg=6y@tcy_1d$0((dp_kj8&nljt2@l&uf^6-id^c_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 DATABASES = {
     'default': {
